539f0e6745f9a7b28688e3d0a74e8c24c6faaabd
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-09 11:35:23 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from os import system # chala ja bhosdike madarchod
+from os import system # chala ja yaar
 system ("git clone https://github.com/ULTRA-OP/ULTRA-X/tree/new && cd ULTRA-X && python3 -m ULTRA")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
